deeb122316de5b4dfaa53bf065c7a31cc2d98184
cshabanic <mrsh13610@gmail.com>
2020-07-10 15:32:51 +0430

    add controller

--- Makefile
+++ Makefile
@@ -1,2 +1,6 @@
 shell:
 	docker exec -it sf2_php bash
+down:
+	docker-compose down
+up:
+	docker-compose up -d
--- code/src/AppBundle/Controller/LuckyController.php
+++ code/src/AppBundle/Controller/LuckyController.php
@@ -2,20 +2,61 @@
 // src/AppBundle/Controller/LuckyController.php
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Product;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
-     * @Route("/lucky/number")
+     * @Route("/lucky/number", name="number")
      */
     public function numberAction()
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('lucky/number.html.twig', array(
+            'number' => $number,
+        ));
+    }
+
+    /**
+     * @Route("/create")
+     */
+    public function createAction()
+    {
+        $product = new Product();
+        $product->setName('Keyboard');
+        $product->setPrice(19.99);
+        $product->setDescription('Ergonomic and stylish!');
+
+        $entityManager = $this->getDoctrine()->getManager();
+
+        // tells Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($product);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$product->getId());
+    }
+
+    /**
+     * @Route("/get/{productId}")
+     */
+    public function getProductAction(int $productId)
+    {
+        $product = $this->getDoctrine()
+                        ->getRepository(Product::class)
+                        ->find($productId);
+
+        if (!$product) {
+            throw $this->createNotFoundException(
+                'No product found for id '.$productId
+            );
+        }
+
+        return new Response('Saved new product with id '.$product->getName());
     }
 }
