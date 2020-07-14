dae54e36fef1838ce4fce20bcdedabb39846dec6
cshabanic <mrsh13610@gmail.com>
2020-07-14 08:20:35 +0430

    add stash changes

--- code/app/AppKernel.php
+++ code/app/AppKernel.php
@@ -16,6 +16,9 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Yoda\EventBundle\YodaEventBundle(),
+            new CoreBundle\CoreBundle(),
+            new Blog\CoreBundle\BlogCoreBundle(),
+            new Blog\ModelBundle\BlogModelBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
