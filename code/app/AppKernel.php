50da64e3b9a32dd2258470381dc72d31785cbbef
azibom <azibom@azibom.com>
2020-08-15 12:53:01 +0430

    Make the first step of project

--- code/app/AppKernel.php
+++ code/app/AppKernel.php
@@ -15,7 +15,8 @@ class AppKernel extends Kernel
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new Yoda\EventBundle\YodaEventBundle(),
+            new Blog\CoreBundle\CoreBundle(),
+            new Blog\ModelBundle\ModelBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
--- code/src/Yoda/EventBundle/YodaEventBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Yoda\EventBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class YodaEventBundle extends Bundle
-{
-}
